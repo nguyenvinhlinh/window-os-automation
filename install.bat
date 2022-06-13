cb9422f5b161d1e137907cd5ffde7c61fe7bd3f7
Nguyen Vinh Linh <nguyenvinhlinh93@gmail.com>
2022-06-13 10:44:58 +0700

    first commit

---
+++ install.bat
@@ -0,0 +1,12 @@
+winget install -e --id Git.Git             --accept-package-agreements  --accept-source-agreements
+winget install -e --id JanDeDobbeleer.OhMyPosh --accept-package-agreements  --accept-source-agreements
+winget install -e --id GNU.Emacs           --accept-package-agreements  --accept-source-agreements
+winget install -e --id GnuPG.Gpg4win       --accept-package-agreements  --accept-source-agreements
+winget install -e --id Postman.Postman     --accept-package-agreements  --accept-source-agreements
+winget install -e --id Microsoft.Skype     --accept-package-agreements  --accept-source-agreements
+winget install -e --id Dropbox.Dropbox     --accept-package-agreements  --accept-source-agreements
+winget install -e --id Discord.Discord     --accept-package-agreements  --accept-source-agreements
+winget install -e --id Microsoft.PowerToys --accept-package-agreements  --accept-source-agreements
+winget install -e --id Cloudflare.Warp     --accept-package-agreements  --accept-source-agreements
+winget install -e --id Microsoft.Teams     --accept-package-agreements  --accept-source-agreements
+winget install -e --id Valve.Steam         --accept-package-agreements  --accept-source-agreements
